311085d532496c23197ba57fc2fcd5cc9fd16a67
Haider Khan <haiderak@students.cs.ubc.ca>
2021-09-07 00:46:53 -0700

    Add movement functionality to paddle.

--- paddle.py
+++ paddle.py
@@ -1,18 +1,15 @@
 import pygame
 import sprite
+import window
 
 
 # Class representing the paddle in the brick breaker game. (inherits Sprite class)
 class Paddle(sprite.Sprite):
 
-    def __init__(self, surface, width, height, initial_x, initial_y):
+    def __init__(self, surface, width, height, initial_x, initial_y, screen):
         # Attributes
         super().__init__(surface, width, height, initial_x, initial_y)
-        self.surface = surface
-        self.width = 6
-        self.height = 40
-        self.initialX = 0
-        self.initialY = 0
+        self.window = screen
         self.velocity = 0  # 0 initially
         self.accel = 7
 
@@ -20,21 +17,53 @@ class Paddle(sprite.Sprite):
     def draw(self):
         pygame.draw.rect(self.surface, self.color, self.rect)
 
+    def move_left(self):
+        self.velocity = -abs(self.accel)
+        self.x_position += self.velocity
+        self.rect.move_ip(self.velocity, 0)
+
+    def move_right(self):
+        self.velocity = abs(self.accel)
+        self.x_position += self.velocity
+        self.rect.move_ip(self.velocity, 0)
+
+    def stop(self):
+        self.velocity = 0
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        # if A key is pressed
+        if keys[pygame.K_a] and paddle.get_x_pos() >= 0:
+            self.move_left()
+
+        # if D key is pressed
+        if keys[pygame.K_d] and paddle.get_x_pos() + paddle.get_width() <= window.width:
+            self.move_right()
+
+
 # Running the class
 if __name__ == '__main__':
-    import window
-
     pygame.init()
+    # Create window
+    window = window.Window()
+    clock = pygame.time.Clock()
+    # Create paddle
+    paddle = Paddle(window.get_surface(), 40, 6, window.width / 2 - 20, window.height - 20, window)
 
-    # create window
-    w_surface = pygame.display.get_surface()
-    window = window.Window(w_surface)
-    window.create_window()
+    # Update display
+    running = True
+    while running:
+        window.set_fps()
 
-    pygame.display.update()
-    paddle = Paddle(w_surface)
-    paddle.draw()
-    pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        paddle.move()
+        window.get_surface().fill(pygame.Color('black'))
+        paddle.draw()
+        pygame.display.update()
 
     # quit pygame
     pygame.quit()
--- window.py
+++ window.py
@@ -4,35 +4,40 @@ import pygame
 # Class representing the window.
 class Window:
 
-    def __init__(self, surface):
+    def __init__(self):
         # Attributes
-        self.surface = surface
         self.background = pygame.Color('black')
         self.FPS = 60
         self.game_Clock = pygame.time.Clock()
-        self.exit = False
-        self.running = True
         self.width = 500
         self.height = 400
+        self.surface = pygame.display.set_mode((self.width, self.height))
 
-    def create_window(self):
-        pygame.display.set_mode((self.width, self.height))
+        # Set window title
         pygame.display.set_caption('Brick Breaker')
 
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+    def get_surface(self):
+        return self.surface
+
+    def set_fps(self):
+        self.game_Clock.tick(self.FPS)
 
 
 # Running the class
 if __name__ == '__main__':
     pygame.init()
 
-    # create window
-    w_surface = pygame.display.get_surface()
-    window = Window(w_surface)
-    window.create_window()
+    # Create window
+    window = Window()
+
+    # Update display
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            pygame.display.update()
 
     # quit pygame
     pygame.quit()
